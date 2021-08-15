445c1ad8fae57945b21d13ec7c01597697866370
LuieSwift <louiseswift0@gmail.com>
2021-08-15 14:41:31 +0100

    Adding/amending weekly forecast

--- src/Weather.js
+++ src/Weather.js
@@ -63,6 +63,7 @@ export default function Weather(props) {
         </a>
 
         <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
